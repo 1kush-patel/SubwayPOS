a403a9feed9d8d0e94463fe432a7d3795c4feea7
mahnoor khan <mahnoor.khanmasood@gmail.com>
2024-06-23 14:59:54 -0400

    final version

---
+++ SubwayPOS/Constant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubwayPOS
+{
+    class Constant
+    {
+        public static class SqlStatements
+        {
+            public const string authUser = "SELECT USERID,PASSWORD, USERNAME FROM USERS WHERE USERID = @UserID AND PASSWORD = @Password";
+            public const string clockInOutInfoStatement = "SELECT CLOCKIN, CLOCKOUT, DATE, USERID FROM WORKINGHOURS WHERE USERID = @UserId AND DATE = @Date";
+            public const string clockInStatement = "INSERT INTO WORKINGHOURS (USERID, CLOCKIN, CLOCKOUT) VALUES (@Userid," +
+                "@ClockInTime, @ClockOutTime)";
+            public const string clockOutStatement = "UPDATE WORKINGHOURS SET CLOCKOUT = @ClockOutTime WHERE USERID = @UserId";
+            public const string submitDailyReportStatement = "INSERT INTO DAILYSTOREREPORT (USERID, USERNAME, LOOSECURRENCY, COINDESPENSER," +
+                "TOTAL, BREADCOUNT, SALAD, WRAP, TIME) VALUES (@UserId, @UserName, @LooseCurrency, @CoinDespenser, @Total, @BreadCount, @Salad, @Wrap, @Time)";
+            public const string todaysDailyReportStatement = "SELECT * FROM DAILYSTOREREPORT WHERE DATE = @Date";
+            public const string getUserByIdStatement = "SELECT * FROM USERS WHERE USERID = @UserId";
+            public const string insertEmployeeStatement = "INSERT INTO USERS (USERID, PASSWORD, USERNAME) VALUES (@UserId, @Password, @UserName)";
+
+            public const string punchOrderStatement = "INSERT INTO SALESINFO(USERID, USERNAME, DATE, TIME, ORDERITEMS, ACTUALAMOUNT, AMOUNTGIVENBYCUSTOMER," +
+                "RETURNEDAMOUNT, TAXAMOUNT, PAYMENTTYPE) VALUES (@UserId, @UserName, @Date, @Time, @OrderItems, @ActualAmount, @AmountGivenByCustomer, @ReturnedAmount," +
+                "@TaxAmount, @PaymentType)";
+            public static string allSalesRecordsStatement = "SELECT * FROM SALESINFO ORDER BY DATE DESC";
+
+            public static string lastPunchedOrder = "SELECT TOP 1 * FROM SALESINFO ORDER BY DATE DESC";
+
+            public static string deleteEmployeeStatement = "DELETE FROM USERS WHERE USERID = @UserId";
+
+
+
+
+        }
+    }
+}
---
+++ SubwayPOS/Validator.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SubwayPOS
+{
+    public class Validator
+    {
+        public static bool validatePrice(string price)
+        {
+            bool result = decimal.TryParse(price, out _);
+            if(result)
+                return result;
+            MessageBox.Show("Please enter valid amount!","Invalid Value", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+        public static bool isSuperUser(int userId) 
+        {
+            if(userId == 26)
+                return true;
+            return false;
+        
+        }
+
+        public static bool validateAuthCreds(string Id, string PW) 
+        {
+            try
+            {
+                int userId;
+                userId = int.TryParse(Id, out userId) ? int.Parse(Id) : throw new FormatException("userId");
+                int passWord;
+                passWord = int.TryParse(PW, out passWord) ? int.Parse(PW) : throw new FormatException("passWord");
+
+            }
+            catch (System.FormatException ex)
+            {
+                if (ex.Message.Equals("userId"))
+                {
+                    MessageBox.Show("Please enter valid numeric value for User ID.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }
+                else if (ex.Message.Equals("passWord"))
+                {
+                    MessageBox.Show("Please enter valid numeric value for Password.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.GetType());
+                return false;
+            }
+            return true;
+        }
+        public static bool _1minuteCashInValidation(string looseMny, string coinDes, string breadCnt, string saladCnt, string wrapCnt)
+        {
+            try
+            {
+                decimal LooseMoney, CoinMoney, BreadCount;
+                int SaladCount, WrapCount;
+                LooseMoney = decimal.TryParse(looseMny, out LooseMoney) ? decimal.Parse(looseMny) : throw new FormatException("LooseMoney");
+                CoinMoney = decimal.TryParse(coinDes, out CoinMoney) ? decimal.Parse(coinDes) : throw new FormatException("CoinMoney");
+                BreadCount = decimal.TryParse(breadCnt, out BreadCount) ? decimal.Parse(breadCnt) : throw new FormatException("BreadCount");
+                SaladCount = int.TryParse(saladCnt, out SaladCount) ? int.Parse(saladCnt) : throw new FormatException("SaladCount");
+                WrapCount = int.TryParse(wrapCnt, out WrapCount) ? int.Parse(wrapCnt) : throw new FormatException("WrapCount");
+                
+            }
+            catch (System.FormatException ex)
+            {
+                if (ex.Message.Equals("LooseMoney"))
+                {
+                    MessageBox.Show("Please enter valid numeric value for Loose Money.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }
+                else if (ex.Message.Equals("CoinMoney"))
+                {
+                    MessageBox.Show("Please enter valid numeric value for Coin Despensor.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }else if (ex.Message.Equals("BreadCount"))
+                {
+                    MessageBox.Show("Please enter valid numeric value for Breads.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }else if (ex.Message.Equals("SaladCount"))
+                {
+                    MessageBox.Show("Please enter valid numeric value for Salad.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }else if (ex.Message.Equals("WrapCount"))
+                {
+                    MessageBox.Show("Please enter valid numeric value for Wrap.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.GetType());
+                return false;
+            }
+            return true;
+
+        }
+        public static bool validateRegistrationCreds(string Id, string password, string name)
+        {
+            try
+            {
+                int userId;
+                userId = int.TryParse(Id, out userId) ? int.Parse(Id) : throw new FormatException("userId");
+
+                int PassWord;
+                PassWord = int.TryParse(password, out PassWord) ? int.Parse(password) : throw new FormatException("password");
+
+                if (name.Equals(""))
+                    throw new FormatException("name");
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Equals("userId"))
+                {
+                    MessageBox.Show("Please enter valid numeric value for User Id.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }
+                else if (ex.Message.Equals("password"))
+                {
+                    MessageBox.Show("Please enter valid numeric value for Password.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }
+
+                else if (ex.Message.Equals("name"))
+                {
+                    MessageBox.Show("User Name should not be empty!", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }
+                else
+                    MessageBox.Show(ex.GetType() + ex.Message);
+
+            }
+            return true;
+
+        }
+        public static bool validateDeletionCreds(string Id)
+        {
+            try
+            {
+                int userId;
+                userId = int.TryParse(Id, out userId) ? int.Parse(Id) : throw new FormatException("userId");
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Equals("userId"))
+                {
+                    MessageBox.Show("Please enter valid numeric value for User Id.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
